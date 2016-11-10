3cbc8d2a3817bd4aec2cc7ccee0c448341db3afc
Martino Colombo <martino.colombo8@gmail.com>
2016-11-10 12:41:28 +0100

    single column version, with small gap

--- parse_projects.py
+++ parse_projects.py
@@ -2,10 +2,11 @@ import subprocess as sp
 import os
 
 types = ['Marc', 'Oliver', 'Rémy', 'Teaching']
-max_line = 2
+max_line = 1
 n = 0
 ids, y, x, value = 'c(', 'c(', 'c(', 'c('
 tmpY = 0
+corrY = 0.1
 offset = dict(zip(types, range(0, max_line*(len(types)+1), max_line)))
 m = dict(zip(types, [0]*len(types)))
 for linea in open('projects'):
@@ -17,11 +18,11 @@ for linea in open('projects'):
 
     height = float(splat[1])
     ids += ','.join([str(n)]*4) + ','
-    y += ','.join([str(x) for x in [tmpY, tmpY-height, tmpY-height, tmpY]]) + ','
+    y += ','.join([str(x-corrY) for x in [tmpY, tmpY-height, tmpY-height, tmpY]]) + ','
     x += ','.join([str(x + offset[typ]) for x in [m[typ], m[typ], m[typ]+1, m[typ]+1]]) + ','
     value += ','.join(['"' + typ + '"']*4) + ','
 
-    tmpY -= height
+    tmpY -= height + corrY
     n += 1
     m[typ] += 1
     if m[typ] > max_line-1:
@@ -43,7 +44,7 @@ values = data.frame(value = factor(''' + value + '''
 datapoly = merge(values, positions, by=c("id"))
 p = ggplot(datapoly, aes(x=x, y=y)) 
 
-svg(height=6,width=4)
+svg(height=6,width=3.5)
     p + geom_polygon(aes(fill=value, group=id)) + theme_bw()
 dev.off()
 
